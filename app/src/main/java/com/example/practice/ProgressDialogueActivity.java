74ab25f62dff2514c9d49112ed59078180ce202c
manoj <manoj.mopoori@improve10x.com>
2023-01-28 23:23:13 +0530

    modified Progress Dialogue Functionality into ui interface

--- app/src/main/java/com/example/practice/ProgressDialogueActivity.java
+++ app/src/main/java/com/example/practice/ProgressDialogueActivity.java
@@ -2,13 +2,31 @@ package com.example.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import com.example.practice.databinding.ActivityProgressDialogueBinding;
+
 public class ProgressDialogueActivity extends AppCompatActivity {
+
+    private ActivityProgressDialogueBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_progress_dialogue);
+        binding = ActivityProgressDialogueBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        handleProgressDialogueBtn();
+    }
+
+    private void handleProgressDialogueBtn() {
+        binding.closeBtn.setOnClickListener(v -> {
+           ProgressDialog  builder =new ProgressDialog(this);
+            builder.setTitle("ProgressDialogue");
+            builder.setMessage("Do You Want close app");
+            builder.show();
+        });
     }
 }
